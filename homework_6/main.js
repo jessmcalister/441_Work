4f206e25d0830feb9345df5603ca1e11f4e2f622
Jessica McAlister <jessmcalister58@gmail.com>
2021-02-22 19:16:33 -0700

    homework_6
    test

--- homework_6/main.js
+++ homework_6/main.js
@@ -1,48 +1,68 @@
-var imageNames = ["image1", "image2", "image3", "image4,", "image5", "image6", "image7", "image8", "image9", "image10"];
+var imageNames = ["image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8", "image9", "image10"];
 var blankImagePath = "images/question.jpg";
 var firstNumber = -1;
 var secondNumber = -1;
 var score = 0;
 var allFound = 0;
+// JSON declaration
 var player = {"firstname":"", "lastname":"", "age":0, "score":0};
+
+// create a variable with the blank image name
+// create a empty array for the actual images
 var actualImages = new Array();
+
 function printBlanks()
 {
+   // call our random image creation function
     createRandomImageArray();
+    // create a for loop
     for(var i = 0; i < imageNames.length; i++)
     {
+    // iterate through the image tag ids and sets the source
         document.getElementById(imageNames[i]).src= blankImagePath;
     }
+
 }
 
 function createRandomImageArray()
 {
-    var actualImagePath = ["images/dumb.jpg", "images/monstera.png", "images/pearl.jfif", "images/snake.jpg", "images/wondering.jpg"];
+    // create an array of actual images
+    var actualImagePath = ["images/snake.jpg", "images/dumb.jpg", "images/monstera.png", "pearl.jfif", "wondering.jpg"];
+    // create another array to make sure the images only get added twice
     var count = [0,0];
+    // create a while statement to check to see if our actual image array is full
     while(actualImages.length < 10)
     {
+        // get a random number between 0 and the number total number of images that we can choose from
         var randomNumber = Math.floor(Math.random() * actualImagePath.length)
-        if(count[randomNumber] < 2)
+          // create an if statement that says if the total number added is less than 2, then
+        // add the image to the actual image array
+        if(count[randomNumber] < 5)
         {
             actualImages.push(actualImagePath[randomNumber]);
+            // then add one to the array that makes sure only two images can be added
             count[randomNumber] = count[randomNumber] + 1;
         }
     }
 }
+
 function flipImage(number)
 {
+
+    // make the second image appear
     if(firstNumber >= 0)
     {
         secondNumber = number;
         document.getElementById(imageNames[number]).src = actualImages[secondNumber];
     }
-    else if(firstNumber < 0)
+    else if(firstNumber < 0) // make the first image appear
     {
         firstNumber = number;
         document.getElementById(imageNames[firstNumber]).src= actualImages[firstNumber];
 
     }
 
+    // check to see if the images do not match
     if(actualImages[secondNumber] != actualImages[firstNumber] && firstNumber >= 0 && secondNumber >= 0)
     {
         score++;
@@ -60,7 +80,7 @@ function flipImage(number)
         {
             player.score = score;
             localStorage.setItem("playerInfo", JSON.stringify(player));
-            window.location = "indexEnd.html";
+            window.location = "HWExampleEnd.html";
         }
     }
 }
@@ -81,11 +101,12 @@ function addToPlayer()
     var firstName = document.getElementById("txtFirstName").value;
     var lastName = document.getElementById("txtLastName").value;
     var age = document.getElementById("txtAge").value;
+
     player.firstname = firstName;
     player.lastname = lastName;
     player.age = age;
     localStorage.setItem("playerInfo", JSON.stringify(player));
-    window.location = "index.html";
+    window.location = "HWExample.html";
 }
 
 // get the information out of JSON
@@ -103,3 +124,4 @@ function playerInfo()
 
 
 }
+© 2021 GitHub, Inc.
