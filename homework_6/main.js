cd944a65c546a17f8cdcfe59c7fc935e492d9441
Jessica McAlister <jessmcalister58@gmail.com>
2021-02-22 19:17:56 -0700

    homework_6
    lest's see

--- homework_6/main.js
+++ homework_6/main.js
@@ -31,13 +31,13 @@ function createRandomImageArray()
     // create another array to make sure the images only get added twice
     var count = [0,0];
     // create a while statement to check to see if our actual image array is full
-    while(actualImages.length < 10)
+    while(actualImages.length < 4)
     {
         // get a random number between 0 and the number total number of images that we can choose from
         var randomNumber = Math.floor(Math.random() * actualImagePath.length)
           // create an if statement that says if the total number added is less than 2, then
         // add the image to the actual image array
-        if(count[randomNumber] < 5)
+        if(count[randomNumber] < 2)
         {
             actualImages.push(actualImagePath[randomNumber]);
             // then add one to the array that makes sure only two images can be added
