cf8e8234aeeb32ec2cb415863ed1e2e78e02ce6f
Jessica McAlister <jessmcalister58@gmail.com>
2021-02-22 19:08:33 -0700

    homework_6
    test

--- homework_6/main.js
+++ homework_6/main.js
@@ -4,66 +4,45 @@ var firstNumber = -1;
 var secondNumber = -1;
 var score = 0;
 var allFound = 0;
-// JSON declaration
 var player = {"firstname":"", "lastname":"", "age":0, "score":0};
-
-// create a variable with the blank image name
-// create a empty array for the actual images
 var actualImages = new Array();
-
 function printBlanks()
 {
-   // call our random image creation function
     createRandomImageArray();
-    // create a for loop
     for(var i = 0; i < imageNames.length; i++)
     {
-    // iterate through the image tag ids and sets the source
         document.getElementById(imageNames[i]).src= blankImagePath;
     }
-
 }
 
 function createRandomImageArray()
 {
-    // create an array of actual images
     var actualImagePath = ["images/dumb.jpg", "images/monstera.png", "images/pearl.jfif", "images/snake.jpg", "images/wondering.jpg"];
-    // create another array to make sure the images only get added twice
     var count = [0,0];
-    // create a while statement to check to see if our actual image array is full
-    while(actualImages.length < 4)
+    while(actualImages.length < 10)
     {
-        // get a random number between 0 and the number total number of images that we can choose from
         var randomNumber = Math.floor(Math.random() * actualImagePath.length)
-          // create an if statement that says if the total number added is less than 2, then
-        // add the image to the actual image array
         if(count[randomNumber] < 2)
         {
             actualImages.push(actualImagePath[randomNumber]);
-            // then add one to the array that makes sure only two images can be added
             count[randomNumber] = count[randomNumber] + 1;
         }
     }
 }
-
 function flipImage(number)
 {
-
-    // make the second image appear
     if(firstNumber >= 0)
     {
         secondNumber = number;
         document.getElementById(imageNames[number]).src = actualImages[secondNumber];
-
     }
-    else if(firstNumber < 0) // make the first image appear
+    else if(firstNumber < 0)
     {
         firstNumber = number;
         document.getElementById(imageNames[firstNumber]).src= actualImages[firstNumber];
 
     }
 
-    // check to see if the images do not match
     if(actualImages[secondNumber] != actualImages[firstNumber] && firstNumber >= 0 && secondNumber >= 0)
     {
         score++;
@@ -102,7 +81,6 @@ function addToPlayer()
     var firstName = document.getElementById("txtFirstName").value;
     var lastName = document.getElementById("txtLastName").value;
     var age = document.getElementById("txtAge").value;
-
     player.firstname = firstName;
     player.lastname = lastName;
     player.age = age;
